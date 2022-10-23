2de3f9c13f1f9f1b6ad4628fc9869bb7251372fb
SoufianeBayoud <soufiane.bayoud@student.ehb.be>
2022-10-23 23:19:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TeDoen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Title = new System.Windows.Forms.TextBox();
+            this.textBox_Info = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Titel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox_Time = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Title
+            // 
+            this.textBox_Title.Location = new System.Drawing.Point(98, 73);
+            this.textBox_Title.Name = "textBox_Title";
+            this.textBox_Title.Size = new System.Drawing.Size(200, 23);
+            this.textBox_Title.TabIndex = 0;
+            // 
+            // textBox_Info
+            // 
+            this.textBox_Info.Location = new System.Drawing.Point(98, 118);
+            this.textBox_Info.Multiline = true;
+            this.textBox_Info.Name = "textBox_Info";
+            this.textBox_Info.Size = new System.Drawing.Size(200, 142);
+            this.textBox_Info.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(98, 280);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // Titel
+            // 
+            this.Titel.AutoSize = true;
+            this.Titel.Location = new System.Drawing.Point(30, 73);
+            this.Titel.Name = "Titel";
+            this.Titel.Size = new System.Drawing.Size(33, 15);
+            this.Titel.TabIndex = 3;
+            this.Titel.Text = "Titel:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Informatie:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tijd:";
+            // 
+            // checkBox_Time
+            // 
+            this.checkBox_Time.AutoSize = true;
+            this.checkBox_Time.Location = new System.Drawing.Point(98, 309);
+            this.checkBox_Time.Name = "checkBox_Time";
+            this.checkBox_Time.Size = new System.Drawing.Size(46, 19);
+            this.checkBox_Time.TabIndex = 6;
+            this.checkBox_Time.Text = "Tijd";
+            this.checkBox_Time.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 37);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Taak Toevoegen";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 37);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Volgende Taak";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(334, 161);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 37);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Taak is afgewerkt";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(334, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 37);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Zet Achteraan";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox_Time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Titel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox_Info);
+            this.Controls.Add(this.textBox_Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox_Title;
+        private TextBox textBox_Info;
+        private DateTimePicker dateTimePicker1;
+        private Label Titel;
+        private Label label2;
+        private Label label1;
+        private CheckBox checkBox_Time;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ TeDoen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeDoen
+{
+    internal class TeDoen
+    {
+        private static int Id;
+        private DateTime? Tijdstip;
+        private string Titel;
+        private string[] Informatie;
+
+        public TeDoen(string Titel, string[] Informatie, DateTime Tijdstip)
+        {
+            this.Titel = Titel;
+            this.Informatie = Informatie;
+            if(Tijdstip >= DateTime.Now)
+            {
+                this.Tijdstip = Tijdstip;
+            }
+            else
+            {
+                this.Tijdstip = null;
+            }
+            Id++;
+        }
+
+        public override string ToString()
+        {
+            string text = "";
+
+            foreach (string line in Informatie)
+            {
+                text += line.ToString() + "\n";
+
+            }
+
+            if (Tijdstip == null)
+            {
+                return "Id: " + Id + "\n Titel : " + Titel + "\n Informatie : " + text;
+            }
+            else
+            {
+                return "Tijdstip: " + Tijdstip + "\n Id: " + Id +  "\n Titel : " + Titel + "\n Informatie : " + text;
+            }
+
+        }
+
+
+
+
+    }
+}
